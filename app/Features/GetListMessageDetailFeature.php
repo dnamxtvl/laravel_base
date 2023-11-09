f47bae33568332bf5d64bb80e2bc975adb25a0e5
dnamxtvl <kakitani2000@gmail.com>
2023-11-09 10:44:13 +0700

    first commit

--- app/Features/GetListMessageDetailFeature.php
+++ app/Features/GetListMessageDetailFeature.php
@@ -24,7 +24,6 @@ class GetListMessageDetailFeature
      */
     public function handle(): JsonResponse
     {
-
         (new CheckValidUserCanSendMessageOrBlockOperation(toUserId: $this->toUserId))->handle();
 
         (new ChangeStatusOfMessageJob(
